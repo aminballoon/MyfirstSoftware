10a0c007adffb4e95c2c0a0a38c477c9ba699fa0
TsBeNz <42747938+TsBeNz@users.noreply.github.com>
2019-10-11 15:25:01 +0700

    enter

--- BackEnd/Frist.py
+++ BackEnd/Frist.py
@@ -3,3 +3,11 @@ print("Sirawit")
 print("Balloon")
 print("Balloon22")
 print("Balloon 47")
+
+def test(input = 0):
+    if input == 1:
+        print("True")
+    
+    else:
+        print("False")
+test(0)
---
+++ BackEnd/calculation_time/Poisson_distribution.py
@@ -0,0 +1,2 @@
+# Poisson distribution
+# https://home.kku.ac.th/nikom/prob_distribution_nk2557.pdf
