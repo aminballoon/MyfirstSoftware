7b0c67adba1ece89a020a8dded331723cf7919e7
lloonl <bal21077@gmail.com>
2019-10-22 14:42:41 +0700

    Update q.py

--- BackEnd/Queqe class/q.py
+++ BackEnd/Queqe class/q.py
@@ -1,21 +1,21 @@
 import time;
 class qry:
     def __init__(self):
-        self.type = []
-        self.num = []
-        self.timeusage = []
-        self.timeuser = []
-        self.timerr = []
-        self.que = []
-        self.wait = []
-        self.count = []
+        self.type = [] #This show the Type of the bank that have been added
+        self.num = []  #This is a wait area of tested queue number
+        self.timeusage = []#This is shown day month year time of each queue
+        self.timeuser = [] #This show start and stop time of each queue
+        self.time_used = [] #This show the time used in each queue
+        self.que = [] #This shown the queue that waiting
+        self.wait = [] #This is the  wait list of queue that get service
+        self.count = [] #This is a wait area of counting queue
     def addtype(self,x):
         self.type.append(x)
         self.num.append([])
         self.que.append([])
         self.timeusage.append([])
         self.timeuser.append([])
-        self.timerr.append([])
+        self.time_used.append([])
         self.wait.append([])
         self.count.append(0)
         while len(self.num[len(self.type) - 1]) < 3:
@@ -25,7 +25,7 @@ class qry:
             c = self.type.index(x)
             self.timeusage[c].append([])
             self.timeuser[c].append([])
-            self.timerr[c].append([])
+            self.time_used[c].append([])
             self.timeusage[c][int(self.num[c-1][0]) * 100+int(self.num[c-1][1]) * 10 + int(self.num[c-1][2])].append(time.asctime(time.localtime(time.time())))
             self.timeuser[c][int(self.num[c-1][0]) * 100+int(self.num[c-1][1]) * 10 + int(self.num[c-1][2])].append(time.time())
             self.num[c-1][2] += 1
@@ -45,7 +45,7 @@ class qry:
                 c = self.type.index(o[0])
                 self.timeusage[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(time.asctime(time.localtime(time.time())))
                 self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(time.time())
-                self.timerr[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][1] - self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][0])
+                self.time_used[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][1] - self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][0])
 
                 break
     def nex(self,x):
@@ -57,7 +57,7 @@ class qry:
                 o = list(self.wait[self.type.index(x)].pop(0))
                 self.timeusage[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(time.asctime(time.localtime(time.time())))
                 self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(time.time())
-                self.timerr[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][1] -self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][0])
+                self.time_used[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1].append(self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][1] -self.timeuser[c][int(o[1]) * 100 + int(o[2]) * 10 + int(o[3]) - 1][0])
             self.count[self.type.index(x)] += 1
 class couter:
     def __init__(self):
