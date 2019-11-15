c25892eb3e23cc292ee08739c99fa61dcd184644
TsBeNz <42747938+TsBeNz@users.noreply.github.com>
2019-11-15 12:32:22 +0700

    update back end

--- BackEnd/Real_function/time_calculation.py
+++ BackEnd/Real_function/time_calculation.py
@@ -1,4 +1,29 @@
-class calculation:
-    static_data = []
-    def __init__(self):
-        pass
+class calculations:
+    def __init__(self,name,ticket_type="",counter_type="",before_queuein={}):
+        self.ticket_type = ticket_type
+        self.counter_type = counter_type 
+        self.before_queue = before_queuein
+        self.name = name
+
+    def on_update(self,input = ""):
+        buffer = 0
+        if(input in self.before_queue):
+            del self.before_queue[input]
+            for i in self.before_queue:
+                buffer += self.before_queue[i]
+            return buffer,self.before_queue
+        return -1,-1
+
+
+if __name__ == '__main__':
+    static_data = {"a":100,"b":100,"c":100,"d":100,"e":100,"f":100}
+    tt = calculations(name = "A000",ticket_type="ticket_a",counter_type="counter_a",before_queuein = static_data)
+    a,b = tt.on_update("a") 
+    print(static_data)
+    k = calculations(name = "A001",ticket_type="ticket_a",counter_type="counter_a",before_queuein = static_data)
+    print(k.before_queue)
+    a,b = tt.on_update("b")
+    print(k.before_queue)
+    a,b = k.on_update("b")
+    print(tt.before_queue)
+    print(k.before_queue)
