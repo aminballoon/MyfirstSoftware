53833762c1bcc374a33854d831f6e0904e328b96
TsBeNz <42747938+TsBeNz@users.noreply.github.com>
2019-11-16 00:31:22 +0700

    Revert "enter"
    This reverts commit af0ad65ace33080252c0c3a8d0cc477b17d19865.

---
+++ BackEnd/calculation_time/Poisson_distribution.py
@@ -0,0 +1,27 @@
+# Poisson distribution
+# https://home.kku.ac.th/nikom/prob_distribution_nk2557.pdf
+# Cerate By TsBeNz 
+
+import firebase_admin
+
+class time_avg:
+    def __init__(self,type_input=""):
+        self.Type = type_input
+        self.List_Time_Used = []
+        self.Avg_time = 0
+    
+    def calculation_avg_time(self):
+        buffer = 0
+        for i in (self.List_Time_Used):
+            buffer += i
+        return buffer/len(self.List_Time_Used)
+
+if __name__ == '__main__':
+    import timeit
+    import time
+    start = timeit.default_timer()
+    stop = timeit.default_timer()
+    print("\nTime ", stop - start)
+    print(time.time())
+    time.sleep(10)
+    print(time.time())
