f7aae425c39e6a3e6ded5d56c323fbae784f311b
TsBeNz <42747938+TsBeNz@users.noreply.github.com>
2019-11-18 21:16:59 +0700

    update backend final!!!!!!!!!!!

--- Final_Solfware/Branch_backend/counter.py
+++ Final_Solfware/Branch_backend/counter.py
@@ -35,19 +35,22 @@ class counter:
             db.collection(self.Branch).document('Data').update({"Avg_" + str(Queue_Push["Type"]): ((int(data["Avg_" + str(Queue_Push["Type"])])*9)+Estimated_Time)/10 ,"Count_"+str(Queue_Push["Type"]):data["Count_"+str(Queue_Push["Type"])]-1})
             Wait_time = int(Queue_Push["Queue_Time"]) - int(Queue_Push["Start_Time"])
             for i in data_queue:
-                listin.append(data_queue[i][0])
-                if (i[0:1] == Queue_Push["No"][0:1] and i != Queue_Push):
+                if (i[0:1] == Queue_Push["No"][0:1] and i != Queue_Push["No"]):
+                    k=1
+                    listin.append(data_queue[i][0])
                     for j in data["counter_"+str(i[0:1]).lower()]:
-                        for z in range(1,len(data_queue[i])):
-                            if Queue_Push["Type"] == j:
-                                print(str(i)+"\t"+str(j)+" eiei in condition")
-                                listin.append(data_queue[i][z]-1)
-                            else:
-                                listin.append(data_queue[i][z])
-                print(listin)
-                diccc[i]=listin
-                listin = []
-            db.collection(self.Branch).document('Queue').update(diccc)
+                        if Queue_Push["Type"] == j:
+                            print("data_queue["+str(i)+"]["+str(k)+"]-1 ------>"+str(data_queue[i][k]-1))
+                            listin.append(data_queue[i][k]-1)
+                        else:
+                            print("data_queue["+str(i)+"]["+str(k)+"] ------>"+str(data_queue[i][k]))
+                            listin.append(data_queue[i][k])
+                        k+=1
+                    diccc[i]=listin
+                    print(diccc)
+                    listin = []
+            if(diccc != {}):
+                db.collection(self.Branch).document('Queue').update(diccc)
             db.collection(self.Branch).document('Queue').update({data["Last_"+self.type]: firestore.DELETE_FIELD})
             db.collection(self.Branch).document('QueuePush').collection("ticket").document(data_queue[data["Last_"+self.type]][0]).update({'Status': 1, 'Estimated_Time': Estimated_Time, 'Wait_Time': Wait_time ,'Stop_Time': time_now})
 
